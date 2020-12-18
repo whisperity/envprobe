3f3e9893b1fef5aabd9348da7fd817854229cb82
Whisperity <whisperity@gmail.com>
2020-12-18 21:46:15 +0000

    Factor out the shortcut logic and write tests for it

---
+++ src/envprobe/commands/shortcuts.py
@@ -0,0 +1,111 @@
+"""
+Implements a logic that maps shortcut calls of envprobe (such as "ep A=b") to
+expanded command invocations.
+"""
+
+# Map certain shortcut characters to different actual commands.
+__SHORTCUT_CHARS = {'+': "add",
+                    '-': "remove",
+                    '?': "get",
+                    '!': "set",
+                    '=': "set",
+                    '^': "undefine",
+                    '{': "load",  # < would be interpreted by shell. :(
+                    '}': "save",  # > would be interpreted by shell. :(
+                    '%': "diff",
+                    }
+
+
+def transform_subcommand_shortcut(argv, registered_commands):
+    """
+    Envprobe's most important functionality and selling point is the ease of
+    operation on getting and setting environmental variables. This is why the
+    user having to type `envprobe add PATH /foo/bar` is too verbose. This
+    method transforms the given `argv` argument vector (usually
+    :function:`sys.argv`) with some common expansions for ease of use.
+
+    :param:`registered_commands` should contain the list of
+    :namespace:`argparse` subcommands. This ensures that resolution to simply
+    getting a variable name that collides with a registered command won't.
+    """
+
+    if len(argv) < 2 or (len(argv) == 2 and argv[1] in ['-h', '--help']):
+        # Don't do anything if the argument vector does not contain at least
+        # one subcommand, or if the user is asking for help.
+        return argv
+
+    should_translate = False
+    command = argv[1]
+
+    if command in __SHORTCUT_CHARS:
+        # Sometimes, users might enter "envprobe + PATH /foo/bar" instead of
+        # "envprobe +PATH /foo/bar" which is the "expected" usage. Fix up these
+        # cases.
+        argv = [argv[0],
+                command + (argv[2] if len(argv) >= 3 else '')] + argv[3:]
+        command = argv[1]
+        should_translate = True
+    else:
+        # Sometimes, users might enter "envprobe PATH + /foo/bar" which
+        # should be a shortcut for "envprobe PATH+ /foo/bar".
+        if len(argv) >= 3 and argv[2] in __SHORTCUT_CHARS:
+            argv = [argv[0], argv[1] + argv[2]] + argv[3:]
+            command = argv[1]
+            should_translate = True
+
+    if len(argv) == 2 and '=' in command:
+        # If the user says "envprobe VAR=VAL", understand it as "envprobe
+        # VAR= VAL" which will be translated into a setter action.
+        parts = command.split('=')
+        variable_name, value = parts[0], parts[1]
+        argv = [argv[0], 'set', variable_name, value]
+
+    if not should_translate and \
+            any([command.startswith(c) or command.endswith(c)
+                 for c in __SHORTCUT_CHARS]):
+        # If no joining of "command letter to variable" was done, the user
+        # might have entered the shortcut as originally "intended":
+        # "envprobe +PATH /foo/bar".
+        should_translate = True
+    elif len(argv) == 2 and command not in registered_commands \
+            and not any([command.startswith(c) or command.endswith(c)
+                         for c in __SHORTCUT_CHARS]):
+        # Shortcut "envprobe PATH" to "envprobe get PATH", if a seemingly
+        # valid variable name (not colliding with a normal command of envprobe)
+        # is given.
+        argv = [argv[0], 'get', command]
+
+    if not should_translate:
+        return list(map(lambda s: s.strip(), argv))
+
+    # Expand the letters into their actual command counterparts.
+    action, variable_name = None, None
+    if command[0] in __SHORTCUT_CHARS:
+        action = [__SHORTCUT_CHARS[command[0]]]
+        variable_name = command[1:]
+    elif command[-1] in __SHORTCUT_CHARS:
+        if command[-1] == '+':
+            # Only "add" is a position-capable action, in which case the '+'
+            # at the end mean suffix, and at the beginning means prefix.
+            action = [__SHORTCUT_CHARS[command[-1]], '--position', '-1']
+        else:
+            action = [__SHORTCUT_CHARS[command[-1]]]
+        variable_name = command[:-1]
+
+    if len(argv) == 4 and (
+            (argv[2] == '' and argv[3] == variable_name) or
+            (argv[2] == variable_name and argv[3] == '')):
+        # If the users enter 'ep VAR=""' the variable name would get
+        # duplicated below, so we change it to the entered empty string.
+        argv = argv[:2] + ['']
+
+    # Cut out the empty parts from the translated argv, thus forbidding simply
+    # saying "envprobe ?" or "envprobe =" by itself.
+    old_argv_tail = argv[2:]
+    argv = [argv[0]] + action
+    if variable_name:
+        argv.append(variable_name)
+    for e in filter(lambda x: x, old_argv_tail):  # x is truthy
+        argv.append(e)
+
+    return list(map(lambda s: s.strip(), argv))
---
+++ test/unit_tests/envprobe/commands/test_shortcuts.py
@@ -0,0 +1,117 @@
+# Note that these tests do not concern whether the rewritten shortcut, such as
+# "ep undefine FOO bar" is a legit call or not, only testing the rewriting
+# logic from the symbol shortcut to the full command.
+
+import shlex
+
+from envprobe.commands.shortcuts import transform_subcommand_shortcut
+
+
+def target(arg_string):
+    """
+    Calls the method under test as if :param:`arg_string` was a command-line
+    invocation, without the invoked command's name.
+    """
+
+    args_to_pass = ["envprobe"] + shlex.split(arg_string)
+    result = transform_subcommand_shortcut(args_to_pass, ["__RESERVED"])
+    assert(result[0] == "envprobe")
+    return result[1:]
+
+
+def test_basic():
+    assert(target("") == [])
+    assert(target("-h") == ["-h"])
+    assert(target("--help") == ["--help"])
+
+
+def test_add():
+    assert(target("+") == ["add"])
+    assert(target("+ PATH") == ["add", "PATH"])
+    assert(target("+ PATH foo") == ["add", "PATH", "foo"])
+    assert(target("+PATH foo") == ["add", "PATH", "foo"])
+
+    assert(target("+ PATH foo bar") == ["add", "PATH", "foo", "bar"])
+    assert(target("+PATH foo bar") == ["add", "PATH", "foo", "bar"])
+
+    assert(target("PATH +") == ["add", "--position", "-1", "PATH"])
+    assert(target("PATH + foo") == ["add", "--position", "-1", "PATH", "foo"])
+    assert(target("PATH+ foo") == ["add", "--position", "-1", "PATH", "foo"])
+
+    assert(target("PATH + foo bar") ==
+           ["add", "--position", "-1", "PATH", "foo", "bar"])
+    assert(target("PATH+ foo bar") ==
+           ["add", "--position", "-1", "PATH", "foo", "bar"])
+
+
+def test_remove():
+    assert(target("-") == ["remove"])
+    assert(target("- PATH") == ["remove", "PATH"])
+    assert(target("- PATH foo") == ["remove", "PATH", "foo"])
+    assert(target("-PATH foo") == ["remove", "PATH", "foo"])
+
+    assert(target("- PATH foo bar") == ["remove", "PATH", "foo", "bar"])
+    assert(target("-PATH foo bar") == ["remove", "PATH", "foo", "bar"])
+
+
+def test_get():
+    assert(target("?") == ["get"])
+    assert(target("? PATH") == ["get", "PATH"])
+    assert(target("? PATH foo") == ["get", "PATH", "foo"])
+    assert(target("?PATH foo") == ["get", "PATH", "foo"])
+
+    assert(target("PATH") == ["get", "PATH"])
+
+
+def test_get_on_varname_colliding_with_reserved_command():
+    # Note that subcommands that are considered registered valid commands
+    # should NOT be rewritten to a "get" call.
+    assert(target("__RESERVED") != ["get", "__RESERVED"])
+    assert(target("__RESERVED") == ["__RESERVED"])
+
+
+def test_set():
+    assert(target("!") == ["set"])
+    assert(target("! PATH") == ["set", "PATH"])
+    assert(target("! PATH foo") == ["set", "PATH", "foo"])
+    assert(target("!PATH foo") == ["set", "PATH", "foo"])
+
+    assert(target("=") == ["set"])
+    assert(target("= PATH") == ["set", "PATH"])
+    assert(target("= PATH foo") == ["set", "PATH", "foo"])
+    assert(target("=PATH foo") == ["set", "PATH", "foo"])
+
+    assert(target("=") == ["set"])
+    assert(target("PATH=") == ["set", "PATH"])
+    assert(target("PATH = foo") == ["set", "PATH", "foo"])
+    assert(target("PATH=foo") == ["set", "PATH", "foo"])
+
+    assert(target("PATH=\"\"") == ["set", "PATH"])
+
+
+def test_undefine():
+    assert(target("^") == ["undefine"])
+    assert(target("^ PATH") == ["undefine", "PATH"])
+    assert(target("^ PATH foo") == ["undefine", "PATH", "foo"])
+    assert(target("^PATH foo") == ["undefine", "PATH", "foo"])
+
+
+def test_load():
+    assert(target("{") == ["load"])
+    assert(target("{ PATH") == ["load", "PATH"])
+    assert(target("{ PATH foo") == ["load", "PATH", "foo"])
+    assert(target("{PATH foo") == ["load", "PATH", "foo"])
+
+
+def test_save():
+    assert(target("}") == ["save"])
+    assert(target("} PATH") == ["save", "PATH"])
+    assert(target("} PATH foo") == ["save", "PATH", "foo"])
+    assert(target("}PATH foo") == ["save", "PATH", "foo"])
+
+
+def test_diff():
+    assert(target("%") == ["diff"])
+    assert(target("% PATH") == ["diff", "PATH"])
+    assert(target("% PATH foo") == ["diff", "PATH", "foo"])
+    assert(target("%PATH foo") == ["diff", "PATH", "foo"])
