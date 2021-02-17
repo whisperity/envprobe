ed6f1ee851fbe0375d26867c4b61d108c6963684
Whisperity <whisperity@gmail.com>
2021-02-17 19:36:05 +0100

    [NFC] Add the copyright notice to the headers of files too

--- src/envprobe/shell/__init__.py
+++ src/envprobe/shell/__init__.py
@@ -1,3 +1,19 @@
+# Copyright (C) 2018 Whisperity
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from .core import CapabilityError, FakeShell, Shell, \
     get_class, get_current_shell, get_kind, get_known_kinds, \
     load, load_all, register_type
